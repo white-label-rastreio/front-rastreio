dc7f00bcc8a634f32a01a282888518340fb31133
Railson25 <railsonsantiago123@gmail.com>
2024-05-18 18:47:14 -0300

    refactor(oldProject): Delete
    This commit will delete old project in react

--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-// Define a constate api para a chamada das requisições
-export const api = axios.create({
-  // Define a URL base para as requisições à API
-  baseURL: 'https://api.rastreou.com/',
-});
